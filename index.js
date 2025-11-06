e4ad5c2f8e131ca375e69b6b39576035881f28b7
casimmons-byte <casimmons@anamosa.k12.ia.us>
2025-11-06 19:14:34 +0000

    index Q length

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
+let quoteLength = quote.length;
+let twentySecondLetter = quote[21];
+let output = `the length of the quote is ${quoteLength} and the twenty-second letter in the quote is ${twentySecondLetter}`;
+let output = `the length of the quote is ${quoteLength} and the twenty-second letter in the quote is [${twentySecondLetter}]`;
+
+document.getElementById("result").innerHTML = output;
